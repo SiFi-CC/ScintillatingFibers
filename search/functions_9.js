e5e6fd4e9e28c5dc2d4ce98e68d60b915e337858
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2018-04-13 15:53:31 +0200

    new version of documentation

--- search/functions_9.js
+++ search/functions_9.js
@@ -4,5 +4,6 @@ var searchData=
   ['_7esfdata',['~SFData',['../classSFData.html#ae2ccfbc2c1474764c7d57a83efca720b',1,'SFData']]],
   ['_7esfmc',['~SFMC',['../classSFMC.html#a32c83e03b459909c6ac550acae631ba1',1,'SFMC']]],
   ['_7esfpeakfinder',['~SFPeakFinder',['../classSFPeakFinder.html#aaa6b6a8c933120a2a378e085d863cd54',1,'SFPeakFinder']]],
+  ['_7esftimingres',['~SFTimingRes',['../classSFTimingRes.html#a65154371d895e67251ed4670d96af46b',1,'SFTimingRes']]],
   ['_7etimeconst',['~TimeConst',['../classTimeConst.html#ad5b9c7df62a443f3481a94a9a3a64481',1,'TimeConst']]]
 ];
