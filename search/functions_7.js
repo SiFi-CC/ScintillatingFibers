794b1913e385f8297340d1e3ec895cc8998c2f29
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2018-07-03 15:27:22 +0200

    new version of documentation

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['reset',['Reset',['../classSFData.html#a8aac9ef9696a6bcc2ebedd27bc176054',1,'SFData::Reset()'],['../classSFMC.html#aab1f005bb58adcfed87eed6433c60a10',1,'SFMC::Reset()'],['../classSFTimeConst.html#a91df9c6d2e4a9ff0ff446028663cffa8',1,'SFTimeConst::Reset()'],['../classTFit.html#a2d37be63fc5ccd350b6729c61a943f7b',1,'TFit::Reset()']]]
+  ['reset',['Reset',['../classSFData.html#a8aac9ef9696a6bcc2ebedd27bc176054',1,'SFData::Reset()'],['../classSFFitResults.html#af6023f2b86e32d66ec9082b5f0c9d8c5',1,'SFFitResults::Reset()'],['../classSFMC.html#aab1f005bb58adcfed87eed6433c60a10',1,'SFMC::Reset()'],['../classSFTimeConst.html#a91df9c6d2e4a9ff0ff446028663cffa8',1,'SFTimeConst::Reset()'],['../classTFit.html#a2d37be63fc5ccd350b6729c61a943f7b',1,'TFit::Reset()']]]
 ];
