1c57ba58a362ac064ea603d38ebdb62dd358b0ce
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2018-10-22 16:51:13 +0200

    new version of documentation

--- search/functions_2.js
+++ search/functions_2.js
@@ -3,7 +3,7 @@ var searchData=
   ['findpeakrange',['FindPeakRange',['../classSFPeakFinder.html#a2134bbf124e4903c01d2753a6bd2be87',1,'SFPeakFinder']]],
   ['fit',['Fit',['../classSFPeakFinder.html#adc42a0852af5dd52f667c189b83fbbef',1,'SFPeakFinder']]],
   ['fitallsignals',['FitAllSignals',['../classSFTimeConst.html#a8acad00d9f455ce813cda5996b402ea1',1,'SFTimeConst::FitAllSignals(void)'],['../classSFTimeConst.html#a2e6a150a2947a8e6bf0c0ec93d3db501',1,'SFTimeConst::FitAllSignals(int ch)']]],
-  ['fitdecaytime',['FitDecayTime',['../classSFTimeConst.html#a71d68b3f629c11e0ca4f17dd8bc65fc8',1,'SFTimeConst']]],
+  ['fitdecaytimedouble',['FitDecayTimeDouble',['../classSFTimeConst.html#a6b348ccd7af9af6a5ac5af429ecd1764',1,'SFTimeConst']]],
   ['fitsinglesignal',['FitSingleSignal',['../classSFTimeConst.html#af5c47e8e82419bc7e72f44149ce254c9',1,'SFTimeConst']]],
   ['fitsinglespectrumsplit',['FitSingleSpectrumSplit',['../classTFit.html#adfa68850d2e8ca87965b5347c5ce3cf9',1,'TFit']]],
   ['fitsinglespectrumtwosplit',['FitSingleSpectrumTwoSplit',['../classTFit.html#ab5172ee306ce2d14a7d3d9d2edc3e8e3',1,'TFit']]],
