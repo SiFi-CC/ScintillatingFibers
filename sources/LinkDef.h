89690973b38eb49eb948526ac887f90f9ca97b3e
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2019-07-22 14:13:24 +0200

    LinkDef, Doxyfile and CMakeLists updated, executable moved to bin directory, CMakeLists added

--- sources/LinkDef.h
+++ sources/LinkDef.h
@@ -4,16 +4,10 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class SFData+;
-#pragma link C++ class SFMC+;
-#pragma link C++ class BGFit+;
+#pragma link C++ class SFDrawCommands+;
+#pragma link C++ class SFTools+;
 #pragma link C++ class SFPeakFinder+;
+#pragma link C++ class SFData+;
 #pragma link C++ class SFAttenuation+;
-#pragma link C++ class TFit+;
-#pragma link C++ class SFTimingRes+;
-#pragma link C++ class SFTimeConst+;
-#pragma link C++ class SFFitResults+;
-#pragma link C++ class SFLightOutput+;
-#pragma link C++ class SFEnergyResolution+;
 
 #endif
