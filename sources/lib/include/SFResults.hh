7d5a026a180e5016bb94abbdf0eac8fa3253cd2b
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2021-02-24 20:36:07 +0100

    result container class added

---
+++ sources/lib/include/SFResults.hh
@@ -0,0 +1,168 @@
+// *****************************************
+// *                                       *
+// *          ScintillatingFibers          *
+// *              SFResults.hh             *
+// *          Katarzyna Rusiecka           *
+// * katarzyna.rusiecka@doctoral.uj.edu.pl *
+// *          Created in 2021              *
+// *                                       *
+// *****************************************
+
+#ifndef __SFResults_H_
+#define __SFResults_H_ 1
+
+#include <TObject.h>
+#include <TString.h>
+
+#include <iostream>
+#include <map>
+#include <set>
+
+enum SFResultTypeNum
+{
+    //----- SFPeakFinder
+    kPeakConst,
+    kPeakPosition,
+    kPeakSigma,
+
+    //----- SFStabilityMon
+    kAveragePeakPos,
+    
+    //----- SFTemperature
+    kTemp,
+
+    //----- SFAttenuation
+    kLambda,
+    kChi2NDF,
+
+    //----- SFEnergyRes
+    kEnergyRes,
+
+    //----- SFLightOutput
+    kLight,
+
+    //----- SFTimingRes
+    kTimeRes,
+
+    //----- SFPositionRes
+    kPositionRes,
+    
+    //----- SFTimeConst
+    kFastDecay,
+    kSlowDecay,
+    kIFast,
+    kISlow,
+
+    //----- SFReconstruction
+    //----- light attenuation model
+    kS0,      ///< Model parameter: amplitude
+    kMLambda, ///< Model parameter: attenuation length
+    kEtaR,    ///< Model parameter: reflection coefficient right (ch1)
+    kEtaL,    ///< Model parameter: reflection coefficient left (ch0)
+    kKsi,     ///< Model parameter: asymmetry coefficient
+    kLength,  ///< Model parameter: fiber length
+
+    //----- energy reconstruction
+    kAlpha,
+    kEnergyDiffSigma,
+
+    //----- position reconstruction
+    kMLRSlope,
+    kMLROffset,
+};
+
+enum SFResultTypeObj
+{
+    //----- SFPeakFinder
+    kSpectrum,
+    kPeak,
+
+    //----- SFStabilityMon
+    kPeakPosGraph,
+    kSMResidualGraph,
+
+    //----- SFAttenuation
+    kAttGraph,
+    kMLRSigmaGraph,
+
+    //----- SFEnergyRes
+    kEnergyResGraph,
+
+    //----- SFLightOutput
+    kLightGraph,
+
+    //----- SFTimingRes
+    kTimeResGraph,
+    kTimeSigGraph,
+
+    //----- SFPositionRes
+    kPosRecoVsPosGraph,
+    kPosResVsPosGraph,
+    kMLRvsPosGraph,
+    kPRResidualGraph,
+
+    //----- SFReconstruction
+    //----- light attenuation model
+    kPlFun,     ///< Function representing primary light emission (left/ch0)
+    kPrFun,     ///< Function representing primary light emission (right/ch1)
+    kRlFun,     ///< Function representing reflected light component (left/ch0)
+    kRrFun,     ///< Function representing reflected light component (right/ch1)
+    kSlFun,     ///< Function representing total measured signal (left/ch0)
+    kSrFun,     ///< Function representing total measured signal (right/ch1)
+
+    //----- signal & reconstructed direct component
+    kPlRecoFun,
+    kPrRecoFun,
+    kSlVsPosGraph,
+    kSrVsPosGraph,
+    kPlVsPosGraph,
+    kPrVsPosGraph,
+    kSMLRVsPosGraph,
+    kPMLRVsPosGraph,
+
+    //----- energy reconstruction
+    kAlphaGraph,
+    kEnergyRecoGraph,
+    kEnergyRecoFun,
+    kEnergyDiffGraph,
+
+    //----- position reconstruction
+    kMPosRecoVsPosGraph
+};
+
+class SFResults : public TObject
+{
+
+  private:
+    TString                             fName;
+    std::map<SFResultTypeNum, double>   fValues;
+    std::map<SFResultTypeNum, double>   fUncert;
+    std::map<SFResultTypeObj, TObject*> fObjects;
+
+  public:
+    SFResults(TString name);
+    SFResults();
+    ~SFResults();
+
+    void AddResult(SFResultTypeNum id, double value, double error);
+    void AddValue(SFResultTypeNum id, double vlaue);
+    void AddUncertainty(SFResultTypeNum id, double error);
+    void AddObject(SFResultTypeObj id, TObject* obj);
+
+    std::vector<double> GetResult(SFResultTypeNum id);
+    double              GetValue(SFResultTypeNum id);
+    double              GetUncertainty(SFResultTypeNum id);
+    TObject*            GetObject(SFResultTypeObj id);
+
+    TString EnumToString(SFResultTypeNum id);
+    TString EnumToString(SFResultTypeObj id);
+
+    void    SetName(TString name) { fName = name; };
+    TString GetName(void)         { return fName; };
+
+    void Print(void);
+
+    ClassDef(SFResults, 1)
+};
+
+#endif
---
+++ sources/lib/src/SFResults.cc
@@ -0,0 +1,218 @@
+// *****************************************
+// *                                       *
+// *          ScintillatingFibers          *
+// *              SFResults.cc             *
+// *          Katarzyna Rusiecka           *
+// * katarzyna.rusiecka@doctoral.uj.edu.pl *
+// *          Created in 2021              *
+// *                                       *
+// ***************************************** 
+
+#include "SFResults.hh"
+
+ClassImp(SFResults);
+
+//------------------------------------------------------------------
+TString gEnumNamesNum[] = {"kPeakConst", "kPeakPosition", "kPeakSigma", //SFPeakFinder
+                           "kAveragePeakPos", //SFStability
+                           "kTemp", //SFTemperature
+                           "kLambda", "kChi2NDF", //SFAttenuation
+                           "kEnergyRes", //SFEnergyRes
+                           "kLight", //SFLightOutput
+                           "kTimeRes", //SFTimingRes
+                           "kPositionRes", //SFPositionRes
+                           "kFastDecay", "kSlowDecay", "kIFast", "kISlow", //SFTimeConst
+                           "kS0", "kMLambda", "kEtaR", "kEtaL", "kKsi", "kLength", //SFReconstrunction - model
+                           "kAlpha", "kEnergyDiffSigma", //SFReconstruction - energy reco
+                           "kMLRSlope", "kMLROffset" //SFReconstruction - position reco
+                           };
+                       
+TString gEnumNamesObj[] = {"kSpectrum", "kPeak", //SFPeakFinder
+                           "kPeakPosGraph", "kSMResidualGraph", //SFStability
+                           "kAttGraph", "kMLRSigmaGraph", //SFAttenuation
+                           "kEnergyResGraph", //SFEnergyRes
+                           "kLightGraph", //SFLightOutput
+                           "kTimeResGraph", "kTimeSigGraph", //SFTimingRes
+                           "kPosRecoVsPosGraph", "kPosResVsPosGraph", "kMLRvsPosGraph", "kPRResidualGraph", //SFPositionRes
+                           "kPlFun", "kPrFun", "kRlFun", "kRrFun", "kSlFun", "kSrFun" //SFReconstrunction - model
+                           "kPlRecoFun", "kPrRecoFun", "kSlVsPosGraph", "kSrVsPosGraph", "kPlVsPosGraph",
+                           "kPrVsPosGraph", "kSMLRVsPosGraph", "kPMLRVsPosGraph", // SFReconstruction - components
+                           "kAlphaGraph", "kEnergyRecoGraph", "kEnergyRecoFun", "kEnergyDiffGraph", //SFReconstruction - energy reco
+                           "kMPosRecoVsPosGraph" //SFReconstruction - position reco
+                       };
+//------------------------------------------------------------------
+SFResults::SFResults(TString name) : fName(name)
+{
+}
+//------------------------------------------------------------------
+SFResults::SFResults() : fName("results")
+{
+}
+//------------------------------------------------------------------
+SFResults::~SFResults()
+{
+    fValues.clear();
+    fUncert.clear();
+    fObjects.clear();
+}
+//------------------------------------------------------------------
+TString SFResults::EnumToString(SFResultTypeNum id)
+{
+    TString result_name = gEnumNamesNum[id];
+    return result_name;
+}
+//------------------------------------------------------------------
+TString SFResults::EnumToString(SFResultTypeObj id)
+{
+    TString result_name = gEnumNamesObj[id];
+    return result_name;
+}
+//------------------------------------------------------------------
+void SFResults::AddResult(SFResultTypeNum id, double value, double error)
+{
+    if(fValues.count(id) != 0 ||
+       fUncert.count(id) != 0)
+    {
+        std::cerr << "##### Warning in SFResults::AddResults()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " already exists!" << std::endl;
+        return;
+    }
+    
+    fValues.insert(std::pair <SFResultTypeNum, double> (id, value));
+    fUncert.insert(std::pair <SFResultTypeNum, double> (id, error));
+    
+    return;
+}
+//------------------------------------------------------------------
+void SFResults::AddValue(SFResultTypeNum id, double value)
+{
+    if(fValues.count(id) != 0)
+    {
+        std::cerr << "##### Warning in SFResults::AddValue()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " already exists!" << std::endl;
+        return;
+    }
+    
+    fValues.insert(std::pair <SFResultTypeNum, double> (id, value));
+    
+    return;
+}
+//------------------------------------------------------------------
+void SFResults::AddUncertainty(SFResultTypeNum id, double error)
+{
+    if(fUncert.count(id) != 0)
+    {
+        std::cerr << "##### Warning in SFResults::AddUncertainty()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " already exists!" << std::endl;
+        return;
+    }
+    
+    fUncert.insert(std::pair <SFResultTypeNum, double> (id, error));
+    
+    return;
+}
+//------------------------------------------------------------------
+void SFResults::AddObject(SFResultTypeObj id, TObject *obj)
+{
+    if(fObjects.count(id) != 0)
+    {
+        std::cerr << "##### Warning in SFResults::AddObject()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " already exists!" << std::endl;
+        return;
+    }
+    
+    fObjects.insert(std::pair <SFResultTypeObj, TObject*> (id, obj));
+    
+    return;
+}
+//------------------------------------------------------------------
+std::vector <double> SFResults::GetResult(SFResultTypeNum id)
+{
+    std::vector <double> vec(2);
+    
+    if(fValues.count(id) == 0 ||
+       fUncert.count(id) == 0)
+    {
+        std::cerr << "##### Warning in SFResults::GetResult()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " does not exist!" << std::endl;
+        vec[0] = -1;
+        vec[1] = -1;
+    }
+    
+    vec[0] = fValues.find(id)->second;
+    vec[1] = fUncert.find(id)->second;
+    
+    return vec;
+}
+//------------------------------------------------------------------
+double SFResults::GetValue(SFResultTypeNum id)
+{
+    if(fValues.count(id) == 0)
+    {
+        std::cerr << "##### Warning in SFResults::GetValue()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " does not exist!" << std::endl;
+        return -1;
+    } 
+    
+    return fValues.find(id)->second;
+}
+//------------------------------------------------------------------
+double SFResults::GetUncertainty(SFResultTypeNum id)
+{
+    if(fUncert.count(id) == 0)
+    {
+        std::cerr << "##### Warning in SFResults::GetUncertainty()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " does not exist!" << std::endl;
+        return -1;
+    } 
+    
+    return fUncert.find(id)->second;
+}
+//------------------------------------------------------------------
+TObject* SFResults::GetObject(SFResultTypeObj id)
+{
+    if(fObjects.count(id) == 0)
+    {
+        std::cerr << "##### Warning in SFResults::GetObject()!" << std::endl;
+        std::cerr << "Element " << EnumToString(id) << " does not exist!" << std::endl;
+        return nullptr;
+    }
+    
+    return fObjects.find(id)->second;
+}
+//------------------------------------------------------------------
+void SFResults::Print(void)
+{
+    
+    if(fValues.empty() ||
+       fUncert.empty() ||
+       fObjects.empty())
+    {
+        std::cerr << "##### Error in SFResults::Print()!" << std::endl;
+        std::cerr << "Resuts not available!" << std::endl;
+        return;
+    }
+    
+    std::cout << "\n\n-------------------------------------------\n" << std::endl;
+    std::cout << "SFResults object " << fName << "\n" << std::endl;
+    std::cout << "Numerical results:" << std::endl;
+    
+    for(std::map <SFResultTypeNum, double>::iterator i = fValues.begin(); i != fValues.end() ; ++i)
+    {
+        std::cout << EnumToString(i->first) << " = " << i->second << " +/- " << fUncert.find(i->first)->second << std::endl;
+    }
+    
+    std::cout << "\nObjects: " << std::endl;
+    
+    for(std::map <SFResultTypeObj, TObject*>::iterator i = fObjects.begin(); i != fObjects.end() ; ++i)
+    {
+        std::cout << EnumToString(i->first) << ": ";
+        i->second->Print();
+        std::cout << std::endl;
+    }
+    
+    std::cout << "\n-------------------------------------------\n\n" << std::endl;
+    
+    return;
+}
+//------------------------------------------------------------------
